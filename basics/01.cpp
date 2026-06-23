1ef82f52eed53679643cad28b55d2658d0327df7
aman00001767 <amang8324@gmail.com>
2026-06-23 11:01:46 +0530

    day2

---
+++ basics/01.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age;
+    cin>>age;
+    cout<<age<<endl;
+}
---
+++ basics/cond.java
@@ -0,0 +1,27 @@
+package basics;
+import java.util.Scanner;
+
+public class cond {
+    public static void main(String args[]){
+    //     int age,number;
+    //     Scanner in=new Scanner(System.in);
+    //     age=in.nextInt();
+    //     number=in.nextInt();
+    //     if(age>18 && number>10)
+    //         System.out.println("test will be conducted");
+    // }
+    // char grade='a';
+    // switch(grade){
+    //     case 'a':
+    //         System.out.println("excellent");
+    //         break;
+    //     case 'b':
+    //         System.out.println("hdnuj");
+    //         break;
+    //     default:
+    //         System.out.println("invalid");
+    // }
+    int x=10,y=20;
+    System.out.println(x>y?x:y);
+}
+}
