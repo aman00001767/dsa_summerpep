234d51a9aa0c77544facc0ba9198ad522dfe0af8
aman00001767 <amang8324@gmail.com>
2026-06-24 11:04:06 +0530

    day3 part2

--- arrays/binarysearch.java
+++ arrays/binarysearch.java
@@ -1,6 +1,18 @@
 package arrays;
 
 public class binarysearch {
-    static int search()
+    static int search(int arr[],int key,int start,int end){
+        if(start>end) return -1;
+        int mid=(start+end)/2;
+        if(arr[mid]==key) return mid;
+        if(key<arr[mid]) return search(arr, key,start,mid-1);
+        else return search(arr, key, mid+1, end);
+
+
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,6,8,9,12,35,};
+        System.out.println(search(arr, 6, 0, arr.length-1));
+    }
     
 }
